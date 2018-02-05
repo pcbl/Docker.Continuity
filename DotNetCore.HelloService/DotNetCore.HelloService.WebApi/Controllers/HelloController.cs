1e7a67e9040bd714632278b95d04fadd7cf9e697
Pedro Castelo Branco <pedro_castelo@hotmail.com>
2018-02-05 19:46:34 +0100

    V1, no docker support

---
+++ DotNetCore.HelloService/DotNetCore.HelloService.WebApi/Controllers/HelloController.cs
@@ -0,0 +1,18 @@
+﻿using DotNetCore.HelloService.WebApi.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCore.HelloService.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Hello")]
+    public class HelloController : Controller
+    {
+        // GET api/Hello/Pedro
+        [HttpGet("{firstName}")]
+        public Greeting Get(string firstName)
+        {
+            return new Greeting(){Name=firstName};
+        }
+
+    }
+}
---
+++ DotNetCore.HelloService/DotNetCore.HelloService.WebApi/Model/Greeting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCore.HelloService.WebApi.Model
+{
+    public class Greeting
+    {
+        public string Name { get; set; }
+
+        public string Salutation => $"Hello, {Name}!";
+    }
+}
